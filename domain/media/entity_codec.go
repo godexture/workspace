2829d61704abd5b61cf4ce40d6a912fc96159a1b
uni-kakurenbo <kohma1116@gmail.com>
2026-06-16 13:42:14 +0900

    refactor

--- domain/media/entity_codec.go
+++ domain/media/entity_codec.go
@@ -3,9 +3,9 @@ package media
 type CodecID string
 
 const (
-	CodecLPCM CodecID = "lpcm"
-	CodecMPEG CodecID = "mpeg"
-	CodecPCMU CodecID = "pcmu"
-	CodecPCMA CodecID = "pcma"
+	CodecLPCM  CodecID = "lpcm"
+	CodecMPEG  CodecID = "mpeg"
+	CodecPCMU  CodecID = "pcmu"
+	CodecPCMA  CodecID = "pcma"
 	CodecMPEG3 CodecID = "mpeg3"
 )
