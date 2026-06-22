37cf2b96fe08cd9392736cabb2cec4095c42a95e
uni-kakurenbo <kohma1116@gmail.com>
2026-06-22 22:19:47 +0900

    adpcm

--- domain/media/entity_codec.go
+++ domain/media/entity_codec.go
@@ -3,9 +3,12 @@ package media
 type CodecID string
 
 const (
-	CodecLPCM CodecID = "lpcm"
-	CodecMPEG CodecID = "mpeg"
-	CodecPCMU CodecID = "pcmu"
-	CodecPCMA CodecID = "pcma"
-	CodecMP3  CodecID = "mp3"
+	CodecLPCM     CodecID = "lpcm"
+	CodecMPEG     CodecID = "mpeg"
+	CodecPCMU     CodecID = "pcmu"
+	CodecPCMA     CodecID = "pcma"
+	CodecMP3      CodecID = "mp3"
+	CodecMSADPCM  CodecID = "adpcm_ms"
+	CodecIMAADPCM CodecID = "adpcm_ima"
+	CodecGSM      CodecID = "gsm"
 )
