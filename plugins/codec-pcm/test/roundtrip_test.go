46030ec65740b15a8a46c678cff0ee7fff7a6ba6
uni-kakurenbo <kohma1116@gmail.com>
2026-07-13 10:50:16 +0900

    fix: test

--- plugins/codec-pcm/test/roundtrip_test.go
+++ plugins/codec-pcm/test/roundtrip_test.go
@@ -40,19 +40,10 @@ func TestWavRoundtripEncodeMuxDemuxDecode(t *testing.T) {
 		t.Run(profile.Name, func(t *testing.T) {
 			t.Parallel()
 
-			// Resample/reformat input PCM if necessary (e.g. downmix to mono for PCMU/PCMA)
-			pcm := sourcePCM
-			if profile.Attrs.ChannelLayout.ChannelCount() == 1 {
-				pcm = testutil.DownmixToMono(pcm)
-			}
-			if profile.Attrs.SampleRate != 16000 {
-				pcm = testutil.Resample16kTo8k(pcm, profile.Attrs.ChannelLayout.ChannelCount())
-			}
-
 			encode := func(pcm []float32) ([]byte, error) {
 				return bridge.Encode(pcm, profile.Codec, profile.Attrs)
 			}
-			testutil.RunRoundtripEncodeDecode(t, pcm, profile.CompareOptions, encode, bridge.Decode)
+			testutil.RunRoundtripEncodeDecode(t, sourcePCM, profile.CompareOptions, encode, bridge.Decode)
 		})
 	}
 }
