f23a7bee2bf66e988ae070a1225e86a7dd55d8af
uni-kakurenbo <kohma1116@gmail.com>
2026-07-18 00:32:14 +0900

    chore: format

--- internal/adpcm/adpcm_test.go
+++ internal/adpcm/adpcm_test.go
@@ -27,7 +27,7 @@ func TestADPCMRoundtrip(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-	t.Parallel()
+			t.Parallel()
 			sampleRate := 8000
 			numSamples := sampleRate
 			pcm := make([]byte, numSamples*tt.channels*2)
