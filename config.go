9e0031d03962eb930be57830ab59f6d07706e23b
uni-kakurenbo <kohma1116@gmail.com>
2026-07-19 21:28:07 +0900

    refactor: configs

--- config.go
+++ config.go
@@ -1,4 +1,3 @@
-//go:generate go run ../../tools/config-generator -source=internal/domain/config.go -type=EncoderConfig -resolved-type=domain.EncoderConfig -default=domain.DefaultEncoderConfig -import=domain=github.com/godexture/codec-mp3/internal/domain -output=config_encoder.go
-//go:generate go run ../../tools/config-generator -source=internal/domain/config.go -type=DecoderConfig -resolved-type=domain.DecoderConfig -default=domain.DefaultDecoderConfig -import=domain=github.com/godexture/codec-mp3/internal/domain -output=config_decoder.go
+//go:generate go run ../../tools/config-generator -target EncoderConfig -target DecoderConfig
 
 package mp3
