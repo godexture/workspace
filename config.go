aeb9d1b97ddef1f198dbeb1c18a40de10f6231c5
uni-kakurenbo <kohma1116@gmail.com>
2026-07-19 21:28:07 +0900

    refactor: configs

--- config.go
+++ config.go
@@ -1,4 +1,3 @@
-//go:generate go run ../../tools/config-generator -source=internal/encoder.go -type=EncoderConfig -resolved-type=internal.EncoderConfig -default=internal.DefaultEncoderConfig -import=internal=github.com/godexture/codec-pcm/internal -output=config_encoder.go
-//go:generate go run ../../tools/config-generator -source=internal/decoder.go -type=DecoderConfig -resolved-type=internal.DecoderConfig -default=internal.DefaultDecoderConfig -import=internal=github.com/godexture/codec-pcm/internal -output=config_decoder.go
+//go:generate go run ../../tools/config-generator -target EncoderConfig -target DecoderConfig
 
 package pcm
