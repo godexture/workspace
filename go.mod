1fff6c39fb753fd06a32973e233cefd2048f6506
uni-kakurenbo <kohma1116@gmail.com>
2026-07-21 04:02:28 +0900

    feat: add godec CLI commands

--- go.mod
+++ go.mod
@@ -1,3 +1,28 @@
 module github.com/godexture/cli
 
 go 1.26.1
+
+require (
+	github.com/godexture/codec-flac v0.0.0
+	github.com/godexture/codec-mp3 v0.0.0
+	github.com/godexture/codec-pcm v0.0.0
+	github.com/godexture/core v0.0.0
+	github.com/godexture/filter-audio v0.0.0
+	github.com/godexture/format-flac v0.0.0
+	github.com/godexture/format-mp3 v0.0.0
+	github.com/godexture/format-wav v0.0.0
+	github.com/godexture/sdk v0.0.0
+	github.com/spf13/cobra v1.10.2
+)
+
+replace github.com/godexture/codec-flac => ../plugins/codec-flac
+replace github.com/godexture/codec-mp3 => ../plugins/codec-mp3
+replace github.com/godexture/codec-pcm => ../plugins/codec-pcm
+replace github.com/godexture/core => ../core
+replace github.com/godexture/filter-audio => ../plugins/filter-audio
+replace github.com/godexture/format-flac => ../plugins/format-flac
+replace github.com/godexture/format-mp3 => ../plugins/format-mp3
+replace github.com/godexture/format-wav => ../plugins/format-wav
+replace github.com/godexture/sdk => ../pkg
+replace github.com/godexture/metadata-id3 => ../plugins/metadata-id3
+replace github.com/godexture/metadata-vorbiscomment => ../plugins/metadata-vorbiscomment
--- internal/main.go
+++ internal/main.go
@@ -1,1 +1,49 @@
 package cli
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"sort"
+
+	godec "github.com/godexture/core"
+	"github.com/godexture/core/registry"
+	"github.com/spf13/cobra"
+)
+
+func Execute(ctx context.Context, args []string) error {
+	root := newRootCommand()
+	root.SetArgs(args)
+	root.SetContext(ctx)
+	return root.Execute()
+}
+
+func newRootCommand() *cobra.Command {
+	root := &cobra.Command{Use: "godec", SilenceUsage: true}
+	root.AddCommand(newListCommand())
+	root.AddCommand(newConvertCommand())
+	return root
+}
+
+func newListCommand() *cobra.Command {
+	return &cobra.Command{
+		Use: "list",
+		RunE: func(command *cobra.Command, _ []string) error {
+			writeRole(command.OutOrStdout(), "muxers", godec.DefaultMuxerRegistry)
+			writeRole(command.OutOrStdout(), "demuxers", godec.DefaultDemuxerRegistry)
+			writeRole(command.OutOrStdout(), "encoders", godec.DefaultEncoderRegistry)
+			writeRole(command.OutOrStdout(), "decoders", godec.DefaultDecoderRegistry)
+			writeRole(command.OutOrStdout(), "filters", godec.DefaultFilterRegistry)
+			return nil
+		},
+	}
+}
+
+func writeRole[V registry.Manifest](writer io.Writer, title string, values *registry.Registry[V]) {
+	names := values.Names()
+	sort.Strings(names)
+	_, _ = fmt.Fprintf(writer, "%s:\n", title)
+	for _, name := range names {
+		_, _ = fmt.Fprintf(writer, "  %s\n", name)
+	}
+}
