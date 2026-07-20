a1643e2b974e4fbc9d298727912863f6753ff735
uni-kakurenbo <kohma1116@gmail.com>
2026-07-20 11:20:46 +0900

    optimize: replace slower MP3 synthesis SIMD

--- internal/mp3/synthwindow_dispatch_scalar.go
+++
@@ -1,7 +0,0 @@
-//go:build !goexperiment.simd || !amd64
-
-package mp3
-
-func synthWindow(workspace []float32, zLineOffset, index int, window []float32) ([4]float32, [4]float32) {
-	return synthWindowScalar(workspace, zLineOffset, index, window)
-}
