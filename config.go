582bfb2504502ad416194a383c14e3cc76035512
uni-kakurenbo <kohma1116@gmail.com>
2026-07-21 04:14:27 +0900

    refactor: migrate tools to pflag

--- config.go
+++ config.go
@@ -1,3 +1,3 @@
 package filter
 
-//go:generate config-generator -target FormatConfig -target ResampleConfig -target RemixConfig -target GainConfig -target NormalizeConfig -target FadeConfig -target DCOffsetConfig -target TrimConfig
+//go:generate config-generator --target FormatConfig --target ResampleConfig --target RemixConfig --target GainConfig --target NormalizeConfig --target FadeConfig --target DCOffsetConfig --target TrimConfig
