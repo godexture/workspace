95a5893e181b4903950c7c0923d58c334bc722fd
uni-kakurenbo <kohma1116@gmail.com>
2026-07-20 20:47:57 +0900

    fix: remove unavailable mp3 encoder

--- internal/domain/config.go
+++ internal/domain/config.go
@@ -3,9 +3,3 @@ package domain
 type DecoderConfig struct{}
 
 var DefaultDecoderConfig = DecoderConfig{}
-
-type EncoderConfig struct {
-	Bitrate int
-}
-
-var DefaultEncoderConfig = EncoderConfig{Bitrate: 128000}
