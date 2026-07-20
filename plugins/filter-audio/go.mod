a69f6aefda0b9271b63ab707ac35afa7ea6b3ef6
uni-kakurenbo <kohma1116@gmail.com>
2026-07-20 20:10:21 +0900

    feat: add audio filter engine foundation

--- plugins/filter-audio/go.mod
+++ plugins/filter-audio/go.mod
@@ -1,3 +1,12 @@
 module github.com/godexture/filter-audio
 
 go 1.26.1
+
+require (
+	github.com/godexture/core v0.0.0
+	github.com/godexture/sdk v0.0.0
+)
+
+replace github.com/godexture/core => ../../core
+
+replace github.com/godexture/sdk => ../../pkg
---
+++ plugins/filter-audio/internal/gain/engine.go
@@ -0,0 +1,59 @@
+package gain
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/godexture/core/domain/media"
+	"github.com/godexture/filter-audio/internal/audio"
+	"github.com/godexture/filter-audio/internal/config"
+	"github.com/godexture/filter-audio/internal/framequeue"
+)
+
+type Engine struct {
+	factor float32
+	queue  framequeue.Single
+}
+
+func New(config config.GainConfig) (*Engine, error) {
+	if err := config.Validate(); err != nil {
+		return nil, err
+	}
+	return &Engine{factor: float32(math.Pow(10, config.Decibels/20))}, nil
+}
+
+func Apply(samples []float32, factor float32) {
+	for i := range samples {
+		samples[i] *= factor
+	}
+}
+
+func (e *Engine) SendFrame(frame *media.Frame) error {
+	if frame == nil || *frame == nil {
+		return fmt.Errorf("gain received nil frame")
+	}
+	input, ok := (*frame).(*media.AudioFrame)
+	if !ok {
+		return fmt.Errorf("gain expected *media.AudioFrame, got %T", *frame)
+	}
+	if e.factor == 1 {
+		input.Retain()
+		return e.queue.Push(input)
+	}
+	block, err := audio.Decode(frame)
+	if err != nil {
+		return err
+	}
+	for _, channel := range block.Channels {
+		Apply(channel, e.factor)
+	}
+	output, err := audio.Encode(block, input.Format, input.BitsPerSample)
+	if err != nil {
+		return err
+	}
+	return e.queue.Push(output)
+}
+
+func (e *Engine) ReceiveFrame() (*media.Frame, error) { return e.queue.Receive() }
+func (e *Engine) Flush() error                        { e.queue.Flush(); return nil }
+func (e *Engine) Close() error                        { e.queue.Close(); return nil }
