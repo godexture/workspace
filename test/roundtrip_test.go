0b609aeaffa75c14c54fd248b321971e3c3491eb
uni-kakurenbo <kohma1116@gmail.com>
2026-07-21 15:08:57 +0900

    fix: reject incompatible decoders

--- test/roundtrip_test.go
+++ test/roundtrip_test.go
@@ -31,7 +31,11 @@ func TestRoundtrip(t *testing.T) {
 					return wavFormat.NewDemuxerEngine(r, wavFormat.NewDemuxerConfig())
 				},
 				Decode: func(stream media.StreamInfo) engine.DecoderEngine {
-					return pcmCodec.NewDecoderEngine(stream, pcmCodec.NewDecoderConfig())
+					decoder, err := pcmCodec.NewDecoderEngine(stream, pcmCodec.NewDecoderConfig())
+					if err != nil {
+						t.Fatal(err)
+					}
+					return decoder
 				},
 				Encode: func() engine.EncoderEngine {
 					return pcmCodec.NewEncoderEngine(
